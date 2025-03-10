c5ebf619abc026b253b827b0ecd5b5e5024b329c
Carlos Szpiro <carlos.szpiro@outlook.com>
2025-03-10 01:23:04 -0300

    file: edited main.jsx and vite.config

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { HashRouter as Router } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <Router>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </Router>,
-)
+  </StrictMode>
+);
